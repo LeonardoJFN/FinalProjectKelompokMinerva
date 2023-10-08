23e1a6e76b2bab36271734445b002e9d5d6fd772
LeonardoJFN <100654621+LeonardoJFN@users.noreply.github.com>
2023-10-08 22:11:36 +0700

    test2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.finalprojectkelompokminerva"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.finalprojectkelompokminerva"
