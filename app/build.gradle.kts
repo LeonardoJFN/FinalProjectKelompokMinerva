04ade96852320ae991f357b092d1ef3d59d902dc
LeonardoJFN <100654621+LeonardoJFN@users.noreply.github.com>
2023-12-21 22:01:10 +0700

    fix error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,5 +89,8 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.8.1")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("androidx.activity:activity-compose:1.3.1")
+    implementation("androidx.compose.ui:ui:1.1.0")
+    implementation("androidx.compose.material:material:1.1.0")
 
 }
